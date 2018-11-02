d79eed9753712040dad6e3bcee99c20496f4c4a1
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-11-02 21:27:32 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'users/new'
 
-  get 'static_pages/about'
+  #get 'static_pages/help'
 
-  get 'static_pages/contact'
+  #get '/help', to: 'static_pages#help', as: 'helf' #this changes the url from /static_pages/help to/help
+  get '/help', to: 'static_pages#help'
+  # get /help and route it to 'static_pages/help'
+  get '/about', to: 'static_pages#about'
+  
+  get '/contact', to: 'static_pages#contact'
 
+  get '/signup', to: 'users#new'
 
 	root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
